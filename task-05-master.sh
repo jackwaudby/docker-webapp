5a4c5cd80465ea8847f873861e0744a1400b905d
Jack Waudby <jackwaudby@jacks-mbp.home>
2017-12-06 23:13:54 +0000

    calls load generator, creates database in mongoDB and populates with raw data for each container

--- task-05-master.sh
+++ task-05-master.sh
@@ -1,10 +1,12 @@
-# This shell script calls the load generator and then input the data into the mongo db 
-# usage sh ./task-05-master.sh 
+# This shell script that calls the load generator, followed by task-05-pipeline. 
+# This inputs the data into a mongo db database   
+
+# usage: sh ./task-05-master.sh http://localhost:8080/primecheck 10 10 5 load-generator-number
+# returns: a csv file with database names for use in the r script, a csv file with collection names for use in the r script 
 
 #!/bin/bash
 
 sh ./task-03-load-generator.sh $1 $2 $3 $4 $5
-#sh ./task-03-load-generator.sh http://localhost:8080/primecheck 10 10 5
 
 sleep 10s
 
