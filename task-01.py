d88d877d2e8c2c456ab2a1ec1e67977f7103e81a
Jack Waudby <jackwaudby@jacks-mbp.home>
2017-12-06 22:51:49 +0000

    running a docker using the python SDK

--- task-01.py
+++ task-01.py
@@ -1,4 +1,5 @@
 # Python script for pulling and running a container 
+# The image run here is a prime number check: 'nclcloudcomputing/javabenchmarkapp'
 # usage: python task-01.py  
 
 # loading modules 
@@ -7,7 +8,9 @@ import docker
 # creating connection to docker daemon 
 client = docker.from_env()
 
-# running container in detached mode
+# running container in detached mode 
+# setting exposure port to 8080 on the local host
+# container removed when stopped
 container = client.containers.run('nclcloudcomputing/javabenchmarkapp',detach=True,ports={8080:8080},name='primecheck',remove=True)
 
 # print container id to terminal
