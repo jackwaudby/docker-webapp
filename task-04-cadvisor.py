4944874eb3223d124af6a356df5da7c95e7da4b6
Jack Waudby <jackwaudby@jacks-mbp.home>
2017-12-07 19:40:07 +0000

    changed exposure port from 8080:8090

--- task-04-cadvisor.py
+++ task-04-cadvisor.py
@@ -8,7 +8,7 @@ import docker
 client = docker.from_env()
 
 # running container in detached mode
-container = client.containers.run('google/cadvisor',detach=True,ports={'8080/tcp': ('127.0.0.1', 8080)},name='cadvisor',remove=True,volumes={'/': {'bind': '/rootfs', 'mode': 'ro'},'/var/run': {'bind': '/var/run', 'mode': 'rw'},'/sys': {'bind': '/sys', 'mode': 'ro'},'/var/lib/docker/': {'bind': '/var/lib/docker', 'mode': 'ro'}})
+container = client.containers.run('google/cadvisor',detach=True,ports={'8080/tcp': ('127.0.0.1', 8090)},name='cadvisor',remove=True,volumes={'/': {'bind': '/rootfs', 'mode': 'ro'},'/var/run': {'bind': '/var/run', 'mode': 'rw'},'/sys': {'bind': '/sys', 'mode': 'ro'},'/var/lib/docker/': {'bind': '/var/lib/docker', 'mode': 'ro'}})
 
 # print container id to terminal
 print container.id
